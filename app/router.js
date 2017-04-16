fd2d105cbe2a7bed643901242efe905ae978d4fa
Chrysallis <lady_aviendha@hotmail.com>
2017-04-15 17:10:51 -0700

    added base assets, dummy tests and routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('drafts');
 });
 
 export default Router;
---
+++ tests/acceptance/display-books-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'prettyprint/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | display books');
+
+test('should show interactive logo', function (assert) {
+});
+test('should show every book in collection', function (assert) {
+});
+test('should open book for editing when clicked on', function (assert) {
+});
+test('should close book when logo is clicked', function (assert) {
+});
